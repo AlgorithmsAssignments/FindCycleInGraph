35d9baa97b4eb2e7553c154bcbdb56828dc36249
Raim Interiano <rai.interiano@gmail.com>
2017-06-07 11:34:24 -0600

    Find cycle implemented

--- main.cpp
+++ main.cpp
@@ -3,8 +3,43 @@
 
 using namespace std;
 
+bool nodeHasBeenVisited(bool* visitedNodes, int currentNodeIndex)
+{
+	return visitedNodes[currentNodeIndex];
+}
+
+void initVisitedArray(bool* visited, int size)
+{
+	for(int i = 0; i < size; ++i)
+	{
+		visited[i] = false;
+	}
+}
+
 bool hasCycle(int** graph, int size)
 {
+	//graph[fromNode][toNode] = cost
+	int currentNode = -1;
+	int cost = -1;
+	bool visitedNodes[size];
+	initVisitedArray(visitedNodes, size);
+	for(int i = 0; i < size; ++i)
+	{
+		visitedNodes[i] = true;
+		cout<<"Visiting: "<<i<<endl;
+		for(int k = 0; k < size; ++k)
+		{
+			cost = graph[i][k];
+			if(cost != -1)
+			{
+				if(nodeHasBeenVisited(visitedNodes, k))
+				{
+					cout<<"Has been visited: "<<k<<" while on "<<i<<endl;
+					return true;
+				}
+			}
+		}
+	}
     return false;
 }
 
